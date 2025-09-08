5800f2ade0d9967c76ab49d417c6cf82532d8657
Ivan MacOS <fortran.new@gmail.com>
2025-09-08 16:19:55 +0700

    юрист-

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "cfc8d522671655f82a7e972af7fcc432",
+const RESOURCES = {"flutter_bootstrap.js": "57fb0302cdeb1f36b8b83f09ef894d6b",
 "version.json": "fb3581d3456b2a577da8f8f783e4d1af",
 "favicon.ico": "1e897cdbe42a696e62c90a882a004600",
 "index.html": "c8bab5e26df96ff174d26da325fa4bb4",
 "/": "c8bab5e26df96ff174d26da325fa4bb4",
 "privacy/index.html": "d61fefa0a57992ee26fde69da6f7ef73",
 "firebase-messaging-sw.js": "320ea0e1925a08152df6ea5976e6dd22",
-"main.dart.js": "b065e41241c0782e6a73b79732869f7f",
+"main.dart.js": "d446cc81238da5ab9bfa26a1a25b0b97",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "remove_account/index.html": "dd2437e2f7fd53297864af8741b3ece8",
 "icons/Icon-192.png": "5dfce1c81d9190814fbac88dda889cd1",
